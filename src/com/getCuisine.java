85a721082432b7b23b359de62141c11ac2538462
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2021-06-05 13:21:08 +0100

    API accessed to return specific type of cuisine in specific neighbourhood

--- src/com/getCuisine.java
+++ src/com/getCuisine.java
@@ -1,6 +1,25 @@
 package com;
 
-public class getCuisine {
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
+public class getCuisine {
+    static public restaurants[] getCusine(allRestaurants restArray, String cusineInput, String neighInput) {
+        restaurants[] newRest;
+        ArrayList<restaurants> newRestList = new ArrayList<>();
+
+        for (int i = 0; i < restArray.restaurants.length; i++) {
+            if (restArray.restaurants[i].cuisinType.matches(cusineInput)) {
+                if (restArray.restaurants[i].neighborhood.matches(neighInput)) {
+                    newRestList.add(restArray.restaurants[i]);
+                }
+            }
+        }
+        newRest = newRestList.toArray(new restaurants[newRestList.size()]);
+        return newRest;
+
+    }
 
 }
