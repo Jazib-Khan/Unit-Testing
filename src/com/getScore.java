d710304713f7ad36752864c81934d594a18f5e4f
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2021-06-06 15:50:03 +0100

    API accessed to return a list of restaurants with specific DOHMH scores

--- src/com/getScore.java
+++ src/com/getScore.java
@@ -1,4 +1,27 @@
 package com;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+
 public class getScore {
+    public static restaurants[] getScore(allRestaurants restArray, String neighInput) {
+        restaurants[] newRest;
+        ArrayList<restaurants> newRestList = new ArrayList<>();
+
+        //Comparator<restaurants> byScore = Comparator.comparing()
+
+        //Arrays.sort(restArray.restaurants, );
+
+        for (int i = 0; i < restArray.restaurants.length; i++) {
+            if (restArray.restaurants[i].neighborhood.matches(neighInput)) {
+                newRestList.add(restArray.restaurants[i]);
+            }
+        }
+        newRest = newRestList.toArray(new restaurants[newRestList.size()]);
+        return newRest;
+
+    }
 }
