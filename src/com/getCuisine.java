cc303f75cc811ac91787d1ca1769e45ea3abe165
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2021-06-06 15:30:37 +0100

    API is accessed to return a list of restaurants with specific ratings

--- src/com/getCuisine.java
+++ src/com/getCuisine.java
@@ -6,12 +6,12 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class getCuisine {
-    static public restaurants[] getCusine(allRestaurants restArray, String cusineInput, String neighInput) {
+    static public restaurants[] getCuisine(allRestaurants restArray, String cuisineInput, String neighInput) {
         restaurants[] newRest;
         ArrayList<restaurants> newRestList = new ArrayList<>();
 
         for (int i = 0; i < restArray.restaurants.length; i++) {
-            if (restArray.restaurants[i].cuisinType.matches(cusineInput)) {
+            if (restArray.restaurants[i].cuisinType.matches(cuisineInput)) {
                 if (restArray.restaurants[i].neighborhood.matches(neighInput)) {
                     newRestList.add(restArray.restaurants[i]);
                 }
--- src/com/getReviews.java
+++ src/com/getReviews.java
@@ -1,4 +1,31 @@
 package com;
 
+import java.util.ArrayList;
+
 public class getReviews {
+    public static restaurants[] getReviews(allRestaurants restArray, String neighInput, int reviewInput) {
+        restaurants[] newRest;
+        ArrayList<restaurants> newRestList = new ArrayList<>();
+
+        for (int i = 0; i < restArray.restaurants.length; i++) {
+            if (restArray.restaurants[i].neighborhood.matches(neighInput)) {
+                double point = 0;
+                double count = 0;
+                for (int j = 0; j < restArray.restaurants.length; i++) {
+                    double temp = restArray.restaurants[i].reviews[j].rating;
+
+                    point += temp;
+                    count ++;
+
+                }
+                double total = point / count;
+
+                if (reviewInput <= total) {
+                    newRestList.add(restArray.restaurants[i]);
+                }
+            }
+        }
+        newRest = newRestList.toArray(new restaurants[newRestList.size()]);
+        return newRest;
+    }
 }
