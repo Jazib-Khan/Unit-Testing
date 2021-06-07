5f7e2e0f7d5d223c41db4882c9f069ea11bd0f72
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2021-06-07 12:06:14 +0100

    API accessed to return specific restaurants that are located in the vicinity of a hotel in a specific neighbourhood

--- src/com/getHotel.java
+++ src/com/getHotel.java
@@ -1,9 +1,66 @@
 package com;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class getHotel {
     public static restaurants[] getHotel(allRestaurants restArray, String neighInput){
+        restaurants[] newRest;
+        ArrayList<restaurants> newRestList = new ArrayList<>();
+
+        double dif = 0;
+        double lat = 0;
+        double lng = 0;
+
+        for (int i = 0; i < restArray.restaurants.length; i++) {
+            switch (neighInput){
+                case "Manhattan":
+                    dif = calculateDistance(i, 40.752831, -73.985748, restArray);
+                    break;
+                case "Brooklyn":
+                    dif = calculateDistance(i, 40.753990, -73.949240, restArray);
+                    break;
+                case "Queens":
+                    dif = calculateDistance(i, 40.689510, -73.988100, restArray);
+                    break;
+            }
+            restArray.restaurants[i].distance = dif;
+
+            if (restArray.restaurants[i].neighborhood.equals(neighInput)) {
+                newRestList.add(restArray.restaurants[i]);
+            }
+        }
+        newRest = newRestList.toArray(new restaurants[newRestList.size()]);
+
+        Comparator<restaurants> byDis = Comparator.comparing(restaurants::getDistance);
+        Arrays.sort(newRest, byDis);
+
+        return newRest;
 
     }
 
+    public static double calculateDistance(int i, double lat, double lng, allRestaurants restArray) {
+        double totalLat;
+        double totalLng;
+        double total = 0;
+
+        double restLat = restArray.restaurants[i].latlong.lat;
+        double restLng = restArray.restaurants[i].latlong.lng;
+
+        if (restLat < lat) {
+            totalLat = lat - restLat;
+        } else {
+            totalLat = restLat - lat;
+        }
+        if (restLng < lng) {
+            totalLng = lng - restLng;
+        } else {
+            totalLng = restLng - lng;
+        }
+
+        total = totalLat + totalLng;
+        return total;
+    }
 
 }
